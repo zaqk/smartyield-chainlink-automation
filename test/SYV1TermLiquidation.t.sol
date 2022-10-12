b1009cb94a1c928149c2960796e9295726c67159
zaz <10659278+zaqk@users.noreply.github.com>
2022-10-13 00:37:40 +0800

    forked tests only

--- test/SYV1TermLiquidation.t.sol
+++ test/SYV1TermLiquidation.t.sol
@@ -2,77 +2,79 @@
 pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
-
-import {SmartYield} from "./mocks/SmartYield.v1.sol";
-import {UserFactory} from "./lib/UserFactory.sol";
+import {console2} from "forge-std/console2.sol";
 
 import {ISmartYield} from "src/external/ISmartYield.v1.sol";
 import {SYV1TermLiquidation} from "src/SYV1TermLiquidation.sol";
 
+import {UserFactory} from "./lib/UserFactory.sol";
+
 contract SYV1TermLiquidationTest is Test {
 
+  address constant smartYieldAddr = 0xa0b3d2AF5a37CDcEdA1af38b58897eCB30Feaa1A;
+
   SYV1TermLiquidation automation;
-  SmartYield smartYield;
+  ISmartYield smartYield;
 
   // test addresses
-  address addr1;
-  address addr2;
-  address addr3;
+  address addr;
 
-  uint256 defaultEndTime;
+  uint256 startTime;
+  uint256 endTime;
+
+  address nextTerm;
 
   function setUp() public {
-    vm.warp(1000 days); // to avoid under/overflow
-    smartYield = new SmartYield();
+    smartYield = ISmartYield(smartYieldAddr);
     automation = new SYV1TermLiquidation(smartYield);
     
-    address[] memory testAddresses = new UserFactory().create(3);
-    addr1 = testAddresses[0];
-    addr2 = testAddresses[1];
-    addr3 = testAddresses[2];
+    address[] memory testAddresses = new UserFactory().create(1);
+    addr = testAddresses[0];
 
-    defaultEndTime = block.timestamp;
-    smartYield.setActiveTerm(addr1);
-    smartYield.setBondData(defaultEndTime, false);
+    // load current term data
+    (startTime,endTime,,nextTerm,,,) = smartYield.bondData(
+      smartYield.activeTerm()
+    );
   }
 
   function testCorrectness_checkUpkeep() public {
     // assert false is returned when term is still running
-    vm.warp(defaultEndTime - 1 days);
     vm.prank(address(0), address(0));
     (bool isReady,) = automation.checkUpkeep(new bytes(0));
     assertFalse(isReady);
 
     // assert true is returned when term is no longer running
-    vm.warp(defaultEndTime + 1 days);
+    vm.warp(endTime + 1 days);
     vm.prank(address(0), address(0));
     (isReady,) = automation.checkUpkeep(new bytes(0));
     assertTrue(isReady);
 
-    // set active term to liquidated
-    smartYield.setBondData(defaultEndTime, true);
+    // liquidate active term
+    smartYield.liquidateTerm(smartYield.activeTerm());
 
     // assert false is returned when term has already been liquidated
-    vm.warp(defaultEndTime + 1 days);
     vm.prank(address(0), address(0));
     (isReady,) = automation.checkUpkeep(new bytes(0));
     assertFalse(isReady);
   }
 
   function testCorrectness_performUpkeep() public {
-    
-    // expect performUpkeep to call
     bytes memory selector = abi.encodeWithSelector(
       ISmartYield.liquidateTerm.selector,
-      addr1
+      smartYield.activeTerm()
     );
+
+    // expect performUpkeep to call smartYield.liquidateTerm()
+    vm.warp(endTime + 1 days);
     vm.expectCall(address(automation.smartYield()), selector);
     automation.performUpkeep(new bytes(0));
   }
 
-
-
-  
-
+  function testRevert_performUpkeep() public {
+    // expect revert if term is not ready to be liqudated
+    vm.warp(startTime);
+    vm.expectRevert("SmartYield: term hasn't ended");
+    automation.performUpkeep(new bytes(0));
+  }
 
 }
