67434b687ab439f0a70a162032a53025744e7d75
zaz <10659278+zaqk@users.noreply.github.com>
2023-02-23 01:03:10 +0800

    add deploy script

--- script/DeploySY.s.sol
+++ script/DeploySY.s.sol
@@ -3,13 +3,32 @@ pragma solidity ^0.8.15;
 
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
-import {SYTermLiquidation} from "src/SYTermLiquidation.sol";
+import {IQuoterV2} from "uniswap-v3-periphery/contracts/interfaces/IQuoterV2.sol";
+
 import {ISmartYield} from "src/external/ISmartYield.sol";
+import {IVeloRouter} from "src/external/IVeloRouter.sol";
+
+import {SYTermLiquidation} from "src/SYTermLiquidation.sol";
 
 /// @notice Deploy script for SYV1TermLiquidation
 contract DeploySY is Script {
 
+  ISmartYield smartYield = ISmartYield(address(0));
+  IQuoterV2 quoter = IQuoterV2(address(0));
+  IVeloRouter veloRouter = IVeloRouter(address(0));
+  address keeperRegistry = address(0);
+  address owner = address(0);
+
+
   /// @notice The main script entrypoint
-  function run() external {
+  function run() external returns (SYTermLiquidation){
+    vm.broadcast();
+    return new SYTermLiquidation(
+      smartYield,
+      quoter,
+      veloRouter,
+      keeperRegistry,
+      owner
+    );
   }
 }
